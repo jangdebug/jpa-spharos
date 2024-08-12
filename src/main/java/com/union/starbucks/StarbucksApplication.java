761ce30785cc31af668a1784bbed34f61dfb8553
Jason Ahn <beat1103@gmail.com>
2024-08-12 09:59:52 +0900

    [init] spring boot project

---
+++ src/main/java/com/union/starbucks/StarbucksApplication.java
@@ -0,0 +1,13 @@
+package com.union.starbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksApplication.class, args);
+	}
+
+}
